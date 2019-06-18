445377d5cba9d727da334ef1572dd3eae213a6c2
Fadli Al Baihaq <wb-fadlialba539103@antfin.com>
2019-06-18 17:34:30 +0700

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Fadli Al Baihaqi on 18/06/19.
+//  Copyright © 2019 Fadli Al Baihaqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
